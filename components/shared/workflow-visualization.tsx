93c0ce0b4e9360733d31e0eb7b3a6e5ff5968e82
jovandedeic-byte <jovan.dedeic@gamblio.ai>
2026-01-09 11:08:40 +0100

    Add dropdown menu UI and refactor workflow visualization
    Introduced a new dropdown menu component using @radix-ui/react-dropdown-menu. Refactored the workflow visualization sidebar to use a data-driven approach for video selection and simplified the video rendering logic. Minor UI adjustments were made to the resizable navbar for improved positioning. Updated dependencies to include @radix-ui/react-dropdown-menu.

--- components/shared/workflow-visualization.tsx
+++ components/shared/workflow-visualization.tsx
@@ -18,6 +18,12 @@ const WorkflowVisualization = () => {
     { content: "Alerts operators when high-potential players emerge" },
   ];
 
+  const asideDataCards = [
+    { id: 1, title: "Gamblio.mp4" },
+    { id: 2, title: "AI.mp4" },
+    { id: 3, title: "Predict.mp4" },
+  ];
+
   const [selectedVideo, setSelectedVideo] = useState<number>(1);
   return (
     <DashedBorder
@@ -36,119 +42,61 @@ const WorkflowVisualization = () => {
         sides="all"
         className="w-full max-w-5xl px-4 py-8 mb-12 lg:max-w-full"
       >
-        <Image
-          src="/box.svg"
-          alt="Workflow Visualization"
-          width={1200}
-          height={600}
-          className="hidden mx-auto my-8 lg:block"
-        />
-
         <div className="w-full max-w-7xl border border-border h-[600px] rounded-xl overflow-hidden">
           <div className="w-full hidden h-20 border lg:flex rounded-xs justify-end items-center px-4 border-border">
             Header
           </div>
           <div className="flex flex-col-reverse lg:flex-row h-full lg:h-[520px]">
             <aside className="w-full lg:w-1/5 bg-custom-dark overflow-x-auto lg:overflow-x-hidden overflow-y-hidden p-18 flex flex-row lg:flex-col h-fit lg:h-full items-center justify-around gap-10">
-              <Button
-                variant={"ghost"}
-                className="dark:hover:bg-transparent"
-                onClick={() => setSelectedVideo(1)}
-              >
-                {/* <FilePlay
-                  className={`size-12 ${
-                    selectedVideo === 1
-                      ? "text-primary"
-                      : "text-muted-foreground"
-                  }`}
-                /> */}
-                <div className="flex flex-col items-center gap-4 w-36 h-32 p-4 border bg-custom-dark rounded-md">
-                  <div className="w-full h-full flex justify-center items-center bg-neutral-900 rounded-md">
-                    <Aperture
-                      className={`size-6 ${
-                        selectedVideo === 1
-                          ? "text-primary animate-spin"
-                          : "text-muted-foreground"
-                      }`}
-                    />
+              {asideDataCards.map((card) => (
+                <Button
+                  variant={"ghost"}
+                  key={card.id}
+                  className="dark:hover:bg-transparent"
+                  onClick={() => setSelectedVideo(card.id)}
+                >
+                  <div className="flex flex-col items-center gap-4 w-36 h-32 p-4 border bg-custom-dark rounded-md">
+                    <div className="w-full h-full flex justify-center items-center bg-neutral-900 rounded-md">
+                      <Aperture
+                        className={`size-6 ${
+                          selectedVideo === card.id
+                            ? "text-primary animate-spin"
+                            : "text-muted-foreground"
+                        }`}
+                      />
+                    </div>
+                    <p className="text-muted-foreground">{card.title}</p>
                   </div>
-                  <p className="text-muted-foreground">Gamblio.mp4</p>
-                </div>
-              </Button>
-              <Button
-                variant={"ghost"}
-                className="dark:hover:bg-transparent"
-                onClick={() => setSelectedVideo(2)}
-              >
-                {/* <FilePlay
-                  className={`size-12 ${
-                    selectedVideo === 2
-                      ? "text-primary"
-                      : "text-muted-foreground"
-                  }`}
-                /> */}
-                <div className="flex flex-col items-center gap-4 w-36 h-32 p-4 border bg-custom-dark rounded-md">
-                  <div className="w-full h-full flex justify-center items-center bg-neutral-900 rounded-md">
-                    <Aperture
-                      className={`size-6 ${
-                        selectedVideo === 2
-                          ? "text-primary animate-spin"
-                          : "text-muted-foreground"
-                      }`}
-                    />
-                  </div>
-                  <p className="text-muted-foreground">AI.mp4</p>
-                </div>
-              </Button>
-              <Button
-                variant={"ghost"}
-                className="dark:hover:bg-transparent"
-                onClick={() => setSelectedVideo(3)}
-              >
-                {/* <FilePlay
-                  className={`size-12 ${
-                    selectedVideo === 3
-                      ? "text-primary"
-                      : "text-muted-foreground"
-                  }`}
-                /> */}
-                <div className="flex flex-col items-center gap-4 w-36 h-32 p-4 border bg-custom-dark rounded-md">
-                  <div className="w-full h-full flex justify-center items-center bg-neutral-900 rounded-md">
-                    <Aperture
-                      className={`size-6 ${
-                        selectedVideo === 3
-                          ? "text-primary animate-spin"
-                          : "text-muted-foreground"
-                      }`}
-                    />
-                  </div>
-                  <p className="text-muted-foreground">Predict.mp4</p>
-                </div>
-              </Button>
+                </Button>
+              ))}
             </aside>
             <main className="relative w-full lg:w-4/5 h-full">
               {selectedVideo === 1 ? (
-                <HeroVideoDialog
-                  className="hidden dark:block"
-                  animationStyle="from-center"
-                  videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
-                  thumbnailSrc="https://startup-template-sage.vercel.app/hero-dark.png"
-                  thumbnailAlt="Hero Video"
-                />
+                <div className="relative  h-full w-full  overflow-hidden  ">
+                  <iframe
+                    src="https://www.youtube.com/embed/Ubbb1RK7iFs"
+                    title="Hero Video player"
+                    className="size-full"
+                    allowFullScreen
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  ></iframe>
+                </div>
               ) : selectedVideo === 2 ? (
-                <HeroVideoDialog
-                  className="hidden dark:block"
-                  animationStyle="from-center"
-                  videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
-                  thumbnailSrc="https://startup-template-sage.vercel.app/hero-dark.png"
-                  thumbnailAlt="Hero Video"
-                />
+                <div className="relative  h-full w-full  overflow-hidden  ">
+                  <iframe
+                    src="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
+                    title="Hero Video player"
+                    className="size-full"
+                    allowFullScreen
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  ></iframe>
+                </div>
               ) : (
                 <div className="relative  h-full w-full  overflow-hidden  ">
                   <iframe
                     src="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
                     title="Hero Video player"
-                    className="size-full rounded-2xl"
+                    className="size-full"
                     allowFullScreen
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                   ></iframe>
